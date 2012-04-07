2601c5be3fd7e384fd5657d6af632725ada18901
Markus Mohanty <markus.mo@gmx.net>
2012-04-07 12:17:54 +0200

    Console-Main added

---
+++ Code/HotelSoftware/src/hotelsoftware/checkin/console/CheckinOnConsole.java
@@ -0,0 +1,13 @@
+package hotelsoftware.checkin.console;
+
+/**
+ *
+ * @author mohi
+ */
+public class CheckinOnConsole
+{
+    public void main(String args[])
+    {
+        System.out.println("This is a checkin :-D");
+    }
+}
