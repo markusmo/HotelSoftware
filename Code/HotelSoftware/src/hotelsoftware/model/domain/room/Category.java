521d076e8cee40bc6f2687081735c941ae8c996b
Hubert Rall <hubert.rall@studetns.fhv.at>
2012-04-19 19:32:25 +0430

    Room v.1 Hubertw

--- Code/HotelSoftware/src/hotelsoftware/model/domain/room/Category.java
+++ Code/HotelSoftware/src/hotelsoftware/model/domain/room/Category.java
@@ -4,62 +4,99 @@
  */
 package hotelsoftware.model.domain.room;
 
+import hotelsoftware.model.DynamicMapper;
+import hotelsoftware.model.database.parties.DBCompanyType;
+import hotelsoftware.model.database.room.DBRoom;
+import hotelsoftware.model.domain.parties.CompanyType;
+
 import java.math.BigDecimal;
+import java.util.Collection;
 
 /**
- *
+ * 
  * @author Lins Christian (christian.lins87@gmail.com)
  */
-public class Category implements CategoryData
-{
-    private String name;
-    private BigDecimal price;
-    private BigDecimal minPrice;
-    private int bedAmount;
+public class Category implements CategoryData {
+	private Integer id;
+	private String name;
+	private BigDecimal price;
+	private BigDecimal minPrice;
+	private int bedAmount;
 
-    @Override
-    public int getBedAmount()
-    {
-        return bedAmount;
-    }
+	public void setId(Integer id) {
+		if (id == null)
+			this.id = id;
+	}
 
-    public void setBedAmount(int bedAmount)
-    {
-        this.bedAmount = bedAmount;
-    }
+	public Integer getId() {
+		return id;
+	}
 
-    @Override
-    public BigDecimal getMinPrice()
-    {
-        return minPrice;
-    }
+	@Override
+	public int getBedAmount() {
+		return bedAmount;
+	}
 
-    public void setMinPrice(BigDecimal minPrice)
-    {
-        this.minPrice = minPrice;
-    }
+	public void setBedAmount(int bedAmount) {
+		this.bedAmount = bedAmount;
+	}
 
-    @Override
-    public String getName()
-    {
-        return name;
-    }
+	@Override
+	public BigDecimal getMinPrice() {
+		return minPrice;
+	}
 
-    public void setName(String name)
-    {
-        this.name = name;
-    }
+	public void setMinPrice(BigDecimal minPrice) {
+		this.minPrice = minPrice;
+	}
 
-    @Override
-    public BigDecimal getPrice()
-    {
-        return price;
-    }
+	@Override
+	public String getName() {
+		return name;
+	}
 
-    public void setPrice(BigDecimal price)
-    {
-        this.price = price;
-    }
-    
-    
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+	public Category create(String name, BigDecimal price, BigDecimal minprice,
+			int bedAmound) {
+		return new Category(name, price, minprice, bedAmount);
+	}
+
+	private Category(String name, BigDecimal price, BigDecimal minprice,
+			int bedAmount) {
+		this.name = name;
+		this.price = price;
+		this.minPrice = minprice;
+		this.bedAmount = bedAmount;
+	}
+	public static Category getCategoryByName(String name)
+	{
+	Category c = (Category) DBRoomCategory.getCategoryByName(name);	
+	return (Category)DynamicMapper.map(c);
+	}
+	public static Collection<Category> getAllCategorys()
+	{
+        	  Collection<DBRoomCategory> dbc = DBRoomCategory.getAllCategorys();
+              return (Collection<Category>)DynamicMapper.map(dbc);
+	}
+	public Collection<Category> getFreeRooms(start, ende)
+	{
+	Collection<Category> dbc = DBRoomCategory.getFreeRooms(this, start, ende);
+	return (Collection<Category>)DynamicMapper.map(dbc);
+	}
+	public Collection<Room> getAllRooms()
+	{
+		Collection<Room> dbr = DBRoom.getAllRooms(this);
+		return (Collection<Room>)DynamicMapper.map(dbr);
+	}
 }
--- Code/HotelSoftware/src/hotelsoftware/model/domain/room/RoomOption.java
+++ Code/HotelSoftware/src/hotelsoftware/model/domain/room/RoomOption.java
@@ -4,33 +4,53 @@
  */
 package hotelsoftware.model.domain.room;
 
+import hotelsoftware.model.DynamicMapper;
+import hotelsoftware.model.database.parties.DBCompanyType;
 import hotelsoftware.model.database.room.DBRoomoption;
+import hotelsoftware.model.domain.parties.CompanyType;
+
+import java.util.Collection;
 import java.util.List;
 
 /**
- *
+ * 
  * @author Lins Christian (christian.lins87@gmail.com)
  */
-public class RoomOption implements RoomOptionData
-{    
-    private String name;
+public class RoomOption implements RoomOptionData {
+	private Integer id;
+	private String name;
 
-    public RoomOption(String name)
-    {        
-        this.name = name;
-        DBRoomoption.safeNewRoomOption(name);
-    }
+	private RoomOption(String name) {
+		this.name = name;
+		DBRoomoption.safeNewRoomOption(name);
+	}
 
-    @Override
-    public String getName()
-    {
-        return name;
-    }
-    
-    public static List<RoomOption> getRoomOptions (){
-        // convert and return Roomoptions.getRoomoptions();
-        return null;
-    }
-    
-    
+	public RoomOption create(String name) {
+		return new RoomOption(name);
+	}
+
+	@Override
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		if (this.id == null)
+			this.id = id;
+	}
+
+	public static Collection<RoomOption> getRoomOptions() {
+		Collection<RoomOption> dbro = (Collection<RoomOption>) DBRoomoption
+				.getRoomoptions();
+		return (Collection<RoomOption>) DynamicMapper.map(dbro);
+	}
+
 }
