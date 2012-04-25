a988322a5afc3374e9f4238bf7c43ac8bddc8c95
Christian Lins <christian.lins87@gmail.com>
2012-04-25 11:56:14 +0200

    room manyToOne update

--- Code/HotelSoftware/src/hotelsoftware/model/domain/room/Room.java
+++ Code/HotelSoftware/src/hotelsoftware/model/domain/room/Room.java
@@ -1,5 +1,6 @@
 package hotelsoftware.model.domain.room;
 
+import hotelsoftware.model.domain.room.data.RoomOptionData;
 import hotelsoftware.model.domain.room.data.RoomCategoryData;
 import hotelsoftware.model.domain.room.data.RoomStatusData;
 import hotelsoftware.model.domain.room.data.RoomData;
