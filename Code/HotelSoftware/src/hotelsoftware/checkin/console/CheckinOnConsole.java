b48bdb5720b55bf902784b5a4c3d2cf377a9cfdf
Markus Mohanty <markus.mohanty@students.fhv.at>
2012-04-25 13:32:45 +0200

    LoginWindow added
    User, which works:
    username: user
    password: asdf

--- Code/HotelSoftware/src/hotelsoftware/checkin/console/CheckinOnConsole.java
+++ Code/HotelSoftware/src/hotelsoftware/checkin/console/CheckinOnConsole.java
@@ -2,8 +2,13 @@ package hotelsoftware.checkin.console;
 
 import hotelsoftware.util.HibernateUtil;
 import hotelsoftware.model.database.users.DBUser;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 /**
  *
@@ -13,23 +18,42 @@ public class CheckinOnConsole
 {
     public static void main(String args[])
     {
-        
-         System.out.println("This is a checkin :-D");
-        Session session = HibernateUtil.getSessionFactory().openSession();
-        session.beginTransaction();
-        List<DBUser> userlist = session.createCriteria(DBUser.class).list();
-//        DBUser user1 = (DBUser)session.createQuery("from DBUser where id = 1").uniqueResult();
-        
-        session.close();
-        for (DBUser users : userlist) {
-            System.out.println(users.getUsername());
-        }
-        System.out.println("session closed");
-        session.close();
-        for (DBUser users : userlist) {
-            System.out.println(users.getUsername());
-        }
-        System.out.println("session closed");
-        //System.out.println(user1.getPassword());
+//        try
+//        {
+//            System.out.println("This is a checkin :-D");
+//            Session session = HibernateUtil.getSessionFactory().openSession();
+//            Transaction ts = session.beginTransaction();
+//            ts.begin();
+//
+//            MessageDigest coder;
+//            coder = MessageDigest.getInstance("MD5");
+//            String password = new String(coder.digest("asdf".getBytes()));
+//
+//            DBUser user = new DBUser("user", password);
+//            user.setActive(true);
+//            session.save(user);
+//            session.flush();
+//            ts.commit();
+//            session.close();
+//
+//        }
+//        catch (NoSuchAlgorithmException ex)
+//        {
+//            Logger.getLogger(CheckinOnConsole.class.getName()).log(Level.SEVERE, null, ex);
+//        }
+//        List<DBUser> userlist = session.createCriteria(DBUser.class).list();
+////        DBUser user1 = (DBUser)session.createQuery("from DBUser where id = 1").uniqueResult();
+//        
+//        session.close();
+//        for (DBUser users : userlist) {
+//            System.out.println(users.getUsername());
+//        }
+//        System.out.println("session closed");
+//        session.close();
+//        for (DBUser users : userlist) {
+//            System.out.println(users.getUsername());
+//        }
+//        System.out.println("session closed");
+//        //System.out.println(user1.getPassword());
     }
 }
