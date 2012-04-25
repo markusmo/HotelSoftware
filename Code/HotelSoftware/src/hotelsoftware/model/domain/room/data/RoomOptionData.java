25c96d4282731b4322069d5dfe9ea206b7b4b032
Christian Lins <christian.lins87@gmail.com>
2012-04-25 11:56:30 +0200

    data

---
+++ Code/HotelSoftware/src/hotelsoftware/model/domain/room/data/RoomOptionData.java
@@ -0,0 +1,12 @@
+package hotelsoftware.model.domain.room.data;
+
+/**
+ *
+ * @author Lins Christian (christian.lins87@gmail.com)
+ */
+public interface RoomOptionData
+{
+
+    String getName();
+    
+}
