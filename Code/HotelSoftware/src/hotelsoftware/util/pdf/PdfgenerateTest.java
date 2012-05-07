25df208583ffd2272d70fc8da00179187c0e9c8c
Markus Mohanty <markus.mohanty@students.fhv.at>
2012-05-07 17:33:24 +0200

    pdf generator now thread and observable

--- Code/HotelSoftware/src/hotelsoftware/util/pdf/PdfgenerateTest.java
+++ Code/HotelSoftware/src/hotelsoftware/util/pdf/PdfgenerateTest.java
@@ -4,48 +4,31 @@
  */
 package hotelsoftware.util.pdf;
 
-import com.lowagie.text.BadElementException;
-import com.lowagie.text.DocumentException;
 import hotelsoftware.model.domain.invoice.Invoice;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.Observable;
+import java.util.Observer;
 
 /**
  *
  * @author mohi
  */
-public class PdfgenerateTest
+public class PdfgenerateTest implements Observer
 {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args)
     {
-        try
-        {
-            Invoice invoice = Invoice.getInvoiceByInvoiceNumber("05071200000001");
-            PdfGenerator generator = new PdfGenerator();
-            generator.generateInvoicePDF(invoice.getCustomer(), invoice.getInvoiceNumber(), invoice.getInvoiceItems(), invoice.getCreated(), invoice.getExpiration());
-        }
-        catch (FileNotFoundException ex)
-        {
-            Logger.getLogger(PdfgenerateTest.class.getName()).log(Level.SEVERE, null, ex);
-        }   
-        catch (DocumentException ex)
-        {
-            Logger.getLogger(PdfgenerateTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        catch (MalformedURLException ex)
-        {
-            Logger.getLogger(PdfgenerateTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        catch (IOException ex)
-        {
-            Logger.getLogger(PdfgenerateTest.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
+        Invoice invoice = Invoice.getInvoiceByInvoiceNumber("05071200000001");
+        PdfGenerator generator = new PdfGenerator(invoice.getCustomer(), invoice.getInvoiceNumber(), invoice.getInvoiceItems(), invoice.getCreated(), invoice.getExpiration());
+        Thread thread = new Thread(generator);
+        thread.start();
+    }
+
+    @Override
+    public void update(Observable o, Object o1)
+    {
+        System.out.println("done");
     }
 }
