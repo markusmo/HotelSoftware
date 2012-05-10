2eaeb8d01ae9b646bfddd4e173ee1e41855583d7
Hubert Rall <hubert.rall@studetns.fhv.at>
2012-05-11 02:32:32 +0430

    test

--- Code/HotelSoftware/test/hotelsoftware/controller/createinvoice/UltraTest.java
+++ Code/HotelSoftware/test/hotelsoftware/controller/createinvoice/UltraTest.java
@@ -8,6 +8,31 @@ package hotelsoftware.controller.createinvoice;
  *
  * @author Kno
  */
-public class UltraTest {
-    
+class UltraTest {
+
+    public static void main(String[] args) throws asdfException, fdsaException {
+        throw new asdfException(0);
+    }
+}
+
+class asdfException extends Exception {
+
+    public asdfException(int a) throws fdsaException, asdfException {
+        if (a > 9) {
+            throw new fdsaException(a);
+        } else {
+            throw new asdfException(a + 1);
+        }
+    }
+}
+
+class fdsaException extends Exception {
+
+    public fdsaException(int b) throws asdfException, fdsaException {
+        if (b < 0) {
+        } else {
+            System.out.println(b + "");
+            throw new fdsaException(b - 1);
+        }
+    }
 }
