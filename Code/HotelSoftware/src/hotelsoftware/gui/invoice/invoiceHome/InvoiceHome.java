3e8184a0dcac72d806d0d4ed9854c3a08cb0882d
Christian Lins <christian.lins87@gmail.com>
2012-05-14 11:14:55 +0200

    update

--- Code/HotelSoftware/src/hotelsoftware/gui/invoice/invoiceHome/InvoiceHome.java
+++ Code/HotelSoftware/src/hotelsoftware/gui/invoice/invoiceHome/InvoiceHome.java
@@ -16,10 +16,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
-import java.util.List;
 import javax.swing.*;
 
 /**
