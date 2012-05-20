ccba8d0f857d5ab61da6c1b233bf454d3458f7b3
Markus Mohanty <markus.mo@gmx.net>
2012-05-20 23:19:35 +0200

    forgot to add -.-

---
+++ Code/HotelSoftware/src/hotelsoftware/model/domain/users/IPermission.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelsoftware.model.domain.users;
+
+import hotelsoftware.controller.data.users.PermissionData;
+
+/**
+ *
+ * @author mohi
+ */
+public interface IPermission extends PermissionData
+{
+
+    Integer getId();
+
+    String getName();
+
+    int hashCode();
+
+    void setId(Integer id);
+
+    void setName(String permission);
+    
+}
