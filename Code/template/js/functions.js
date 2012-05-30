d3264515bcffebc50b11b2a185727d397dd08dce
Stefan Dunst <stefan@iplace.at>
2012-05-30 14:14:37 +0200

    Template

---
+++ Code/template/js/functions.js
@@ -0,0 +1,25 @@
+$(function()
+{
+	$("#slides").slides({
+		preload: true,
+		play: 5000,
+		pause: 2500,
+		hoverPause: true,
+		effect: "fade",
+		animationStart: function(current){
+			$('.caption').animate({
+				bottom:-35
+			},100);
+		},
+		animationComplete: function(current){
+			$('.caption').animate({
+				bottom:0
+			},200);
+		},
+		slidesLoaded: function() {
+			$('.caption').animate({
+				bottom:0
+			},200);
+		}
+	});
+});
