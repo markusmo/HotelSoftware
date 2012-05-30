403c1c178ce9629b34b3cd1a665a2662f611b5fc
Markus Mohanty <markus.mohanty@students.fhv.at>
2012-05-30 14:03:42 +0200

    Project neu

---
+++ Code/HotelSoftwareOnline/src/java/hotelsoftwareonline/beans/CompanyBean.java
@@ -0,0 +1,57 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelsoftwareonline.beans;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+/**
+ *
+ * @author Markus Mohanty <markus.mo at gmx.net>
+ */
+@ManagedBean(name="company")
+@SessionScoped
+public class CompanyBean
+{
+
+    private AddressBean address;
+    private AddressBean invoiceAddress;
+    private String name;
+    private CompanyTypeBean type;
+
+    public AddressBean getAddress()
+    {
+        return address;
+    }
+
+    public void setAddress(AddressBean address)
+    {
+        this.address = address;
+    }
+
+    public AddressBean getInvoiceAddress()
+    {
+        return invoiceAddress;
+    }
+
+    public void setInvoiceAddress(AddressBean invoiceAddress)
+    {
+        this.invoiceAddress = invoiceAddress;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+    
+    public CompanyBean()
+    {
+    }
+}
---
+++ Code/HotelSoftwareOnline/src/java/hotelsoftwareonline/validator/RoomValidator.java
@@ -0,0 +1,56 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hotelsoftwareonline.validator;
+
+/**
+ *
+ * @author Tobias
+ */
+import hotelsoftware.model.domain.room.IRoom;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.ValidatorException;
+import hotelsoftware.model.domain.room.RoomCategory;
+import java.util.Collection;
+import java.util.Date;
+import hotelsoftware.model.database.manager.RoomManager;
+import hotelsoftware.model.domain.room.IRoomCategory;
+import javax.faces.application.FacesMessage;
+import hotelsoftwareonline.beans.ReservationBean;
+import hotelsoftwareonline.beans.ReservationItemBean;
+
+@FacesValidator("onlinereservation.validator")
+public class RoomValidator implements Validator{
+    
+    Date start;
+    Date end;
+    ReservationBean bean;
+    RoomManager manager;
+    IRoomCategory category;
+    
+    /**
+     * Überprüft ob genügend freie Räume in der gewünschten Kategorie verfügbar sind
+     * @param context nicht benutzt
+     * @param component nicht benutzt
+     * @param value Die Reservation Bean, enthält die nötigen Informationen über die Reservierung
+     * @throws ValidatorException 
+     */
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+        bean = (ReservationBean)value;
+        manager = RoomManager.getInstance();
+        
+        for (ReservationItemBean r : bean.getItems()){
+            category = manager.getCategoryByName(r.getCategory().getName());
+            if (r.getAmount() > category.getFreeRooms(bean.getStartDate(), bean.getEndDate()).size()){
+                throw new ValidatorException(new FacesMessage("Not enough free rooms available in category " + category.getName()));
+            }
+        }
+    }
+    
+}
