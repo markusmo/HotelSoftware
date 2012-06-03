bf196f80fe94dc191a45310fbf10a332b6966214
Jotschgl <johannes.schwendinger@hotmail.com>
2012-06-03 19:28:53 +0200

    165584

--- Code/HotelSoftwareOnline/src/java/hotelsoftwareonline/beans/ReservationItemBean.java
+++ Code/HotelSoftwareOnline/src/java/hotelsoftwareonline/beans/ReservationItemBean.java
@@ -50,22 +50,5 @@ public class ReservationItemBean implements Serializable
         this.category = category;
     }
     
-    /**
-     * gibt die Verpflegungsarten aus
-     * @return eine Arraylist aller Verplegungsarten
-     */
-    public ArrayList<String> getBoardCategories()
-    {
-        Collection<IExtraService> allExtraServices = ServiceManager.getInstance().getAllExtraServices();
-        ArrayList<String> boardCategories = new ArrayList<String>();
-        for (IExtraService extraservice : allExtraServices)
-        {
-            if(extraservice.getServiceType().getName().equals("Food"))
-            {
-                boardCategories.add(extraservice.getName());
-            }
-        }
-        
-        return boardCategories;
-    }
+   
 }
