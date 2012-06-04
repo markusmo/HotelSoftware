8bf7dab0dfeaf3c24d915611681010d60982af40
Markus Mohanty <markus.mohanty@students.fhv.at>
2012-06-04 17:20:50 +0200

    new navigationrules and beans with new functionality

--- Code/HotelSoftwareOnline/src/java/hotelsoftwareonline/controller/CustomerLoginController.java
+++ Code/HotelSoftwareOnline/src/java/hotelsoftwareonline/controller/CustomerLoginController.java
@@ -5,10 +5,13 @@
 package hotelsoftwareonline.controller;
 
 import hotelsoftware.controller.login.LoginCustomerController;
+import hotelsoftware.model.domain.parties.IAddress;
+import hotelsoftware.model.domain.parties.ICompany;
 import hotelsoftware.model.domain.parties.ICustomer;
+import hotelsoftware.model.domain.parties.IPrivateCustomer;
 import hotelsoftware.support.LoginFailureException;
+import hotelsoftwareonline.beans.*;
 import java.io.Serializable;
-import java.util.HashMap;
 
 /**
  *
@@ -16,30 +19,63 @@ import java.util.HashMap;
  */
 public class CustomerLoginController implements Serializable
 {
-    private static HashMap<String,ICustomer> loggedInUsers = new HashMap<String,ICustomer>();
-    
     /**
-     * Logt einen Kunden ein
+     * Logt einen Kunden ein und wandelt alle Eigenschaften auf die Bean Objekte
+     *
      * @param username der Username des Kunden
      * @param password das Passwort des Kunden
      */
-    public void login(String username, String password) throws LoginFailureException
+    public CustomerBean login(String username, String password) throws LoginFailureException
     {
         ICustomer customer = LoginCustomerController.getInstance().login(username, password);
-        loggedInUsers.put(username,customer);
-    }
-    
-    /**
-     * Logt einen Kunden aus
-     * @param username der Username des Kunden
-     */
-    public void logout(String username)
-    {
-        loggedInUsers.remove(username);
-    }
-    
-    public ICustomer getCustomerByUsername(String username)
-    {
-        return loggedInUsers.get(username);
+
+        IAddress invoiceAddress = customer.getInvoiceAddress();
+        IAddress address = customer.getAddress();
+
+        AddressBean invoiceAddressBean = new AddressBean();
+        AddressBean addressBean = new AddressBean();
+
+        invoiceAddressBean.setCity(invoiceAddress.getCity());
+        invoiceAddressBean.setEmail(invoiceAddress.getEmail());
+        invoiceAddressBean.setFax(invoiceAddress.getFax());
+        invoiceAddressBean.setPhone(invoiceAddress.getPhone());
+        invoiceAddressBean.setStreet(invoiceAddress.getStreet());
+        invoiceAddressBean.setZip(invoiceAddress.getZip());
+
+        addressBean.setCity(address.getCity());
+        addressBean.setEmail(address.getEmail());
+        addressBean.setFax(address.getFax());
+        addressBean.setPhone(address.getPhone());
+        addressBean.setStreet(address.getStreet());
+        addressBean.setZip(address.getZip());
+
+        if (customer instanceof IPrivateCustomer)
+        {
+            IPrivateCustomer privateCustomer = (IPrivateCustomer) customer;
+            PrivateCustomerBean privateCustomerBean = new PrivateCustomerBean();
+
+            privateCustomerBean.setFname(privateCustomer.getFname());
+            privateCustomerBean.setLname(privateCustomer.getLname());
+            privateCustomerBean.setGender(privateCustomer.getGender());
+            privateCustomerBean.setUsername(privateCustomer.getUsername());
+            privateCustomerBean.setAddress(addressBean);
+            privateCustomerBean.setInvoiceAddress(invoiceAddressBean);
+            
+            return privateCustomerBean;
+        }
+
+        ICompany company = (ICompany) customer;
+        CompanyBean companyBean = new CompanyBean();
+        CompanyTypeBean type = new CompanyTypeBean();
+
+        type.setName(company.getCompanyType().getName());
+
+        companyBean.setName(company.getName());
+        companyBean.setType(type);
+        companyBean.setUsername(company.getUsername());
+        companyBean.setInvoiceAddress(invoiceAddressBean);
+        companyBean.setAddress(addressBean);
+
+        return companyBean;
     }
 }
